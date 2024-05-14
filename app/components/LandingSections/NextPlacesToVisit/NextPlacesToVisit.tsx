a8f53bfb75e40f4f46b739fbeadfb602a53c9389
Carlos Quesada Umaña <carlosquesadaumana@gmail.com>
2024-05-13 19:24:07 -0500

    Grid styles fixed

--- app/components/LandingSections/NextPlacesToVisit/NextPlacesToVisit.tsx
+++ app/components/LandingSections/NextPlacesToVisit/NextPlacesToVisit.tsx
@@ -4,7 +4,7 @@ const NextPlacesToVisit = () => {
   return (
     <section>
         <h1 className="font-bold text-[36px] mb-4">Next cities to visit</h1>
-        <ul className="grid grid-cols-2 gap-2">
+        <ul className="grid grid-cols-3 gap-2">
           <li>Richmond, Virginia, USA 🇺🇸</li>
           <li>Memphis, Tenessee, USA 🇺🇸</li>
           <li>Washinton Island, Wisconsin, USA 🇺🇸</li>
