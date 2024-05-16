5d94ab40bf92d36719b8a852aec673a0fdd344e4
Carlos Quesada Umaña <carlosquesadaumana@gmail.com>
2024-05-16 18:32:17 -0500

    Hardcoded data and simple template added to countries-traveled page

--- app/(modules)/countries-I-have-visited/page.tsx
+++ app/(modules)/countries-I-have-visited/page.tsx
@@ -1,20 +1,75 @@
+"use client"
 import dynamic from "next/dynamic";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 
-export default async function Page() {
+import {LatLngExpression} from "leaflet";
+
+interface Country {
+    id: number,
+    name: string,
+    posix: number[]
+}
+
+const SELECTED_VISITED_COUNTRY_INITIAL_STATE: Country = {
+    id: 1,
+    name: "Colombia",
+    posix: [4.79029, -75.69003]
+}
+
+export default function Page() {
+
+    const [selectedVisitedCountry, setSelectedVisitedCountry] = useState<Country>(SELECTED_VISITED_COUNTRY_INITIAL_STATE)
+
     const Map = useMemo(() => dynamic(
         () => import('../../components/Map/Map'),
         {
             loading: () => <p>A map is loading</p>,
             ssr: false
         }
-    ), [])
+    ), [selectedVisitedCountry.posix])
+
+
+    const countriesVisitedList = [{
+        id: 1,
+        name: "Colombia",
+        posix: [4.79029, -75.69003]
+    }, {
+        id: 2,
+        name: "Panama",
+        posix: [8.98238, -79.51987]
+    }, {
+        id: 3,
+        name: "Guatemala",
+        posix: [14.63491, -90.50688]
+    }]
+
+    const handleSelectCountry = (countryId: number) => {
+        console.log("alert1")
+        const foundCountry = countriesVisitedList.find(country => country.id === countryId) ?? SELECTED_VISITED_COUNTRY_INITIAL_STATE
+        console.log(foundCountry.name)
+        setSelectedVisitedCountry(foundCountry)
+    }
 
     return (
         <>
         <h2>Countries I have visited</h2>
+
+            <div className="grid grid-cols-3 gap-2">
+                {
+                    countriesVisitedList.map((country) => (
+                        <div 
+                            key={country.id} 
+                            className="p-2 bg-white text-[#000]"
+                            onClick={() => handleSelectCountry(country.id)}
+                        >
+                            <h2>{country.name}</h2>
+                        </div>
+                    ))
+                }
+            </div>
+
             <div className="bg-white-700 mx-auto my-5 w-[100%] h-[480px]">
-                <Map posix={[4.79029, -75.69003]} />
+                <Map posix={selectedVisitedCountry.posix as LatLngExpression} />
             </div>
         </>
     )
