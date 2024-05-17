b95e00614b60c0f268a18761589c49accac81c78
Carlos Quesada Umaña <carlosquesadaumana@gmail.com>
2024-05-16 20:04:31 -0500

    Countries list on landing styles fixed on mobile

--- app/components/LandingSections/NextPlacesToVisit/NextPlacesToVisit.tsx
+++ app/components/LandingSections/NextPlacesToVisit/NextPlacesToVisit.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const NextPlacesToVisit = () => {
   return (
     <section>
-        <h1 className="font-bold text-[36px] mb-4">Next cities to visit</h1>
-        <ul className="grid grid-cols-3 gap-2">
+        <h1 className="text-center sm:text-left font-bold text-[36px] mb-4">Next cities to visit</h1>
+        <ul className="text-center sm:text-left grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-6">
           <li>Richmond, Virginia, USA 🇺🇸</li>
           <li>Memphis, Tenessee, USA 🇺🇸</li>
           <li>Washinton Island, Wisconsin, USA 🇺🇸</li>
