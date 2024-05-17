1bb7a39510df59f32dbf3d4a89e167842126b1c8
Carlos Quesada Umaña <carlosquesadaumana@gmail.com>
2024-05-16 19:48:55 -0500

    Countries list and return arrow styles overlay map

--- app/(modules)/countries-I-have-visited/page.tsx
+++ app/(modules)/countries-I-have-visited/page.tsx
@@ -3,6 +3,8 @@ import dynamic from "next/dynamic";
 import { useMemo, useState } from "react";
 import { LatLngExpression } from "leaflet";
 
+import { useRouter } from 'next/navigation'
+
 import {
     Country,
     SELECTED_VISITED_COUNTRY_INITIAL_STATE,
@@ -21,6 +23,7 @@ export default function Page() {
         }
     ), [selectedVisitedCountry.posix])
 
+    const router = useRouter()
 
     const handleSelectCountry = (countryId: number) => {
         const foundCountry = countriesVisitedList
@@ -34,12 +37,29 @@ export default function Page() {
         <>
             <h2>Countries I have visited</h2>
 
-            <div className="grid grid-cols-3 gap-2">
+            <div
+                className="
+                    p-4
+                    fixed 
+                    bg-[#3a3a3a]
+                    bottom-0 
+                    left-0
+                    z-20 
+                    w-[200%] 
+                    h-[auto] 
+
+                    flex
+                    justify-start
+                    items-center
+                    
+                    overflow-x-scroll
+                "
+            >
                 {
                     countriesVisitedList.map((country) => (
                         <div
                             key={country.id}
-                            className="p-2 bg-white text-[#000]"
+                            className="py-4 px-12 mr-4 bg-white text-[#2d2d2d] rounded-[10px] hover:cursor-pointer hover:bg-[#FFA500] hover:text-[#000] hover:font-bold"
                             onClick={() => handleSelectCountry(country.id)}
                         >
                             <h2>{country.name}</h2>
@@ -48,13 +68,32 @@ export default function Page() {
                 }
             </div>
 
-            <div className="bg-white-700 mx-auto my-5 w-[100%] h-[480px]">
+            <div className="absolute top-0 left-0 bg-white-700 w-[100%] h-[100vh] z-10">
                 <Map
                     posix={
                         selectedVisitedCountry.posix as LatLngExpression
                     }
                 />
+
             </div>
+
+            <div
+                className="
+                    bg-[#00f] 
+                    text-[#fff] 
+                    absolute 
+                    top-0 
+                    right-0 
+                    z-20 
+                    w-[100px] 
+                    h-[100px] 
+                    flex 
+                    justify-center 
+                    items-center 
+                    hover:cursor-pointer
+                "
+                onClick={() => router.push('/')}
+            >Return</div>
         </>
     )
 }
