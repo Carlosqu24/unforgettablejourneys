1ad8029481a96597f64ce705164bda93b4a0849a
Carlos Quesada Umaña <carlosquesadaumana@gmail.com>
2024-05-18 10:34:06 -0500

    Mobile UI added

--- app/components/LandingSections/NextPlacesToVisit/NextPlacesToVisit.tsx
+++ app/components/LandingSections/NextPlacesToVisit/NextPlacesToVisit.tsx
@@ -4,7 +4,7 @@ const NextPlacesToVisit = () => {
   return (
     <section>
         <h1 className="text-center sm:text-left font-bold text-[36px] mb-4">Next cities to visit</h1>
-        <ul className="text-center sm:text-left grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-6">
+        <ul className="text-center sm:text-left grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-2 leading-8">
           <li>Richmond, Virginia, USA 🇺🇸</li>
           <li>Memphis, Tenessee, USA 🇺🇸</li>
           <li>Washinton Island, Wisconsin, USA 🇺🇸</li>
